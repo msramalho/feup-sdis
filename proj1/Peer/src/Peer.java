e14829829bd8d1daaf902984da9e0951257e44ba
msramalho <up201403027@fe.up.pt>
2018-03-03 17:01:38 +0000

    Peer test setup

--- proj1/Peer/src/Peer.java
+++ proj1/Peer/src/Peer.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import java.net.*;
+
 public class Peer {
-    static PeerConfig peerConfig;
+
 
     public static void main(String[] args) {
+        PeerConfig peerConfig;
         try {
             //create peer
             peerConfig = new PeerConfig(args);
@@ -15,6 +19,26 @@ public class Peer {
             System.out.println("initiator");
             String filename = args[7];
             int replicationFactor = Integer.parseInt(args[8]);
+
+            //setup multicast socket and join group
+            byte[] outData = "This is the message".getBytes(); //create message with service port
+            try {
+                DatagramPacket messsageToBroadcast = new DatagramPacket(outData, outData.length, Inet4Address.getByName(args[5]), Integer.parseInt(args[6]));
+                peerConfig.mcBackup.send(messsageToBroadcast);
+            } catch (Exception e) {
+                System.out.println("Unable to send message");
+                e.printStackTrace();
+            }
+            //socket.setSoTimeout(1000);//set the timeout for receive
+            return;
+        }
+
+        try {
+            String message = peerConfig.receiveMulticast();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return;
         }
 
 
