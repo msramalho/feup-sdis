00491d7bc026aa504a0be216c3c392836c1fe37d
msramalho <up201403027@fe.up.pt>
2018-03-07 22:20:35 +0000

    different peer ids in bat file

--- proj1/Peer/initiator_peer.bat
+++ proj1/Peer/initiator_peer.bat
@@ -4,5 +4,5 @@ prompt -------
 mkdir bin
 javac -cp .;gson-2.8.2.jar;./bin/;./ -d bin src/main/*.java src/localStorage/*.java src/util/*.java
 ::Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
-java -cp .;gson-2.8.2.jar;./bin/ main.Peer 1.0 1 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002 penguin.jpg 3
+java -cp .;gson-2.8.2.jar;./bin/ main.Peer 1.0 2 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002 penguin.jpg 3
 ::pause
