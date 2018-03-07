7d6066d8516e97577e3072fd2e9eab24c7674456
msramalho <up201403027@fe.up.pt>
2018-03-07 11:39:15 +0000

    bat updates

--- proj1/Peer/src/peer1.bat
+++ proj1/Peer/src/peer1.bat
@@ -1,6 +1,8 @@
 echo Starting Peer...
+::javac -cp gson-2.8.2.jar Peer.java *.java
+::javac -cp .:gson-2.8.2.jar Peer.java PeerConfig.java InternalState.java LocalChunk.java LocalFile.java StoredChunk.java
 javac Peer.java
 ::Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
-java Peer 1.0 1 8999 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
+java -cp .:gson-2.8.2.jar Peer 1.0 1 8999 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
 ::pause
 echo --------DONE
