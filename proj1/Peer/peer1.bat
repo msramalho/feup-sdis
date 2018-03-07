e96894a48b29d1b40ff8c00ec5f3959d003f9120
msramalho <up201403027@fe.up.pt>
2018-03-07 19:13:11 +0000

    bat file updated for util package

--- proj1/Peer/peer1.bat
+++ proj1/Peer/peer1.bat
@@ -2,7 +2,7 @@
 prompt -------
 @echo on
 mkdir bin
-javac -cp .;gson-2.8.2.jar;./bin/;./ -d bin src/main/*.java src/localStorage/*.java
+javac -cp .;gson-2.8.2.jar;./bin/;./ -d bin src/main/*.java src/localStorage/*.java src/util/*.java
 ::Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
 java -cp .;gson-2.8.2.jar;./bin/ main.Peer 1.0 1 8999 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
 ::pause
