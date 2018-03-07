6c3dc9df1dd3d0929a03ab50c03178534608059d
msramalho <up201403027@fe.up.pt>
2018-03-07 22:21:09 +0000

    message format and clean up

--- proj1/Peer/src/util/Message.java
+++ proj1/Peer/src/util/Message.java
@@ -7,25 +7,23 @@ import java.util.Arrays;
  */
 public class Message {
     private String action;
-    String protocolVersion;
-    int senderId;
-    int fileId;
-    String[] head;
-    String body;
+    private String protocolVersion;
+    private int senderId;
+    private int fileId;
+    private String[] head;
+    public String body;
 
     public Message(byte[] packetMessageBytes) {
-        String packetMessage = new String(packetMessageBytes);
+        String packetMessage = new String(packetMessageBytes).trim();
         try {
             this.parseMessage(packetMessage);
         } catch (Exception e) {
-            System.err.println(String.format("[Message] - not a valid message (%d bytes): %s from PeerConfig", packetMessage.length(), packetMessage));
-            e.printStackTrace();
+            System.err.println(String.format("[Message] - not a valid message (%d bytes): %s from PeerConfig, ignoring", packetMessage.length(), packetMessage));
         }
     }
 
     //<MessageType> <Version> <SenderId> <FileId> [<ChunkNo> <ReplicationDeg>] <CRLF><CRLF>[<Body>]
     private void parseMessage(String packetMessage) {
-        System.out.println(packetMessage);
         String[] parts = packetMessage.split("\r\n\r\n", 2); // split only once
         String[] args = parts[0].split(" ");
         this.action = args[0];
@@ -33,11 +31,17 @@ public class Message {
         this.senderId = Integer.parseInt(args[2]);
         this.fileId = Integer.parseInt(args[3]);
         this.head = Arrays.copyOfRange(args, 4, args.length); // head keeps the optional args
-        if (parts.length == 2) this.body = parts[1]; //save body if it exists
+        if (parts.length == 2) this.body = parts[1].substring(1); //save body if it exists
+//        System.out.println("Message from: " + String.valueOf(this.senderId));
+//        System.out.println(this.body);
     }
 
     public String getAction() {
         return action;
+    }
+
+    public boolean isOwnMessage(int selfId) {
+        return this.senderId == selfId;
     }
 
     public boolean isBackup() {
