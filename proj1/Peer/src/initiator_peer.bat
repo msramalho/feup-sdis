735f81a2bd46310b040b4ffd197bf8df31d13835
msramalho <up201403027@fe.up.pt>
2018-03-07 18:22:02 +0000

    refactored bat files

--- proj1/Peer/src/initiator_peer.bat
+++
@@ -1,6 +0,0 @@
-echo Starting Initiator + Peer
-javac -cp .;gson-2.8.2.jar Peer.java
-::Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort> <filename> <replicationFactor>
-java -cp .;gson-2.8.2.jar Peer 1.0 1 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002 penguin.jpg 3
-::pause
-echo --------DONE
--- proj1/Peer/src/peer1.bat
+++
@@ -1,6 +0,0 @@
-echo Starting Peer...
-javac -cp .;gson-2.8.2.jar Peer.java
-::Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
-java -cp .;gson-2.8.2.jar Peer 1.0 1 8999 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
-::pause
-echo --------DONE
