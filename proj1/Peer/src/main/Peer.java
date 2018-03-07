0800e441e421b4c5e38c5d94f1caaf33ccf349ac
msramalho <up201403027@fe.up.pt>
2018-03-07 19:12:58 +0000

    peer improvements for testing

--- proj1/Peer/src/main/Peer.java
+++ proj1/Peer/src/main/Peer.java
@@ -1,28 +1,21 @@
 package main;
 
-import main.PeerConfig;
-
 import java.io.IOException;
-import java.net.*;
-
 
 public class Peer {
-
-
     public static void main(String[] args) {
 
         PeerConfig peerConfig;
         try {
             peerConfig = new PeerConfig(args);//create peer
         } catch (Exception e) {
+            System.out.println("[Peer] - cannot parse cmd line args");
             System.out.println(e.getMessage());
             return;
         }
 
         //InternalState internalState = new InternalState(peerConfig);
         //internalState.saveStorage();
-
-
 
         //initiator peer, receives <filename> <replicationFactor>
         if (args.length == 11) {
@@ -31,14 +24,9 @@ public class Peer {
             int replicationFactor = Integer.parseInt(args[8]);
 
             //setup multicast socket and join group
-            byte[] outData = "This is the message".getBytes(); //create message with service port
-            try {
-                DatagramPacket messsageToBroadcast = new DatagramPacket(outData, outData.length, Inet4Address.getByName(args[5]), Integer.parseInt(args[6]));
-                peerConfig.mcBackup.send(messsageToBroadcast);
-            } catch (Exception e) {
-                System.out.println("Unable to send message");
-                e.printStackTrace();
-            }
+            byte[] data = "This is the contents of the file".getBytes(); //create message with service port
+            BackupChunkWorker bcWorker = new BackupChunkWorker(peerConfig, data, 1, 3);
+            bcWorker.run();
             //socket.setSoTimeout(1000);//set the timeout for receive
             return;
         }
