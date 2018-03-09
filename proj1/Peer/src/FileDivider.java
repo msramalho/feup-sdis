199c06ababb6065fd60624b86405215b882c3956
diogoalmeidacunha <diogocunha588@gmail.com>
2018-03-09 10:40:30 +0000

    mainFileDivider and fileDivider

---
+++ proj1/Peer/src/FileDivider.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+
+public class FileDivider {
+    String id;
+    String filename; // path + filename in the current file system
+
+
+    public FileDivider() {
+    }
+}
---
+++ proj1/Peer/src/FileDividerMain.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.*;
+
+public class FileDividerMain {
+
+    public static void main(String[] args) {
+        System.out.println("--- [ SERVER ] ---");
+    }
+}
