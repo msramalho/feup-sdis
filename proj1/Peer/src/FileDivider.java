63bf8d28b6ad28c273eebf30021e73bd8ff612d9
diogoalmeidacunha <diogocunha588@gmail.com>
2018-03-13 15:14:38 +0000

    chunk test

--- proj1/Peer/src/FileDivider.java
+++ proj1/Peer/src/FileDivider.java
@@ -1,11 +1,14 @@
 
 import java.util.ArrayList;
+import java.io.*;
 
 public class FileDivider {
     String id;
     String filename; // path + filename in the current file system
 
-
-    public FileDivider() {
+    public FileDivider(File file) {
+        this.id = "111";
+        this.filename = "da";
+        System.out.println("Created");
     }
 }
--- proj1/Peer/src/FileDividerMain.java
+++ proj1/Peer/src/FileDividerMain.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
 import java.net.*;
+import java.io.*;
 
 public class FileDividerMain {
 
     public static void main(String[] args) {
         System.out.println("--- [ SERVER ] ---");
+        FileReader txt = null;
+
+        File file = new File("file.txt");
+        FileDivider createChunk = new FileDivider(file);
+
     }
 }
