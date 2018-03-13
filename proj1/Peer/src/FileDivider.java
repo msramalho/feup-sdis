c834bfab71c7597d1a43136d8b28bbbc023633cf
diogoalmeidacunha <diogocunha588@gmail.com>
2018-03-13 17:13:22 +0000

    file divided into chunks

--- proj1/Peer/src/FileDivider.java
+++ proj1/Peer/src/FileDivider.java
@@ -1,14 +1,58 @@
 
 import java.util.ArrayList;
 import java.io.*;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.zip.CRC32;
+import javax.swing.*;
 
 public class FileDivider {
-    String id;
-    String filename; // path + filename in the current file system
+    int chunk_Size;
+    int chunck_id;
+    int chunk_name;
+    int file_size;
+    byte[] temporary = null;
+    int totalBytesRead = 0;
 
-    public FileDivider(File file) {
-        this.id = "111";
-        this.filename = "da";
-        System.out.println("Created");
+    public FileDivider(File file , int chunk_Size){
+        this.chunk_Size = chunk_Size;
+        file_size = (int) file.length();
+        System.out.println("[ File Received: Length " + file_size + " ]");
+
+        InputStream inStream = null;
+        int totalBytesRead = 0;
+
+        try {
+            inStream = new BufferedInputStream(new FileInputStream(file));
+            System.out.println(inStream);
+            int i = 0;
+            while(totalBytesRead < file_size){
+
+                int bytesRemaining = file_size-totalBytesRead;
+
+                temporary = new byte[chunk_Size]; //Temporary Byte Array
+                try {
+                    int bytesRead = inStream.read(temporary, 0, chunk_Size);
+                    System.out.println("Chunk["+i+"]" + " size: " + bytesRead);
+                    i++;
+                } catch (IOException e){
+                    System.out.println("[ CAN'T READ Chunk ]");
+                }
+
+                totalBytesRead+=chunk_Size;
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("[ CAN'T READ File ]");
+        }
     }
 }
--- proj1/Peer/src/FileDividerMain.java
+++ proj1/Peer/src/FileDividerMain.java
@@ -5,11 +5,11 @@ import java.io.*;
 public class FileDividerMain {
 
     public static void main(String[] args) {
-        System.out.println("--- [ SERVER ] ---");
+        System.out.println("---------------------------- [ SERVER ] -------------------------------");
         FileReader txt = null;
 
-        File file = new File("file.txt");
-        FileDivider createChunk = new FileDivider(file);
+        File file = new File(args[0]);
+        FileDivider createChunk = new FileDivider(file, 64000);
 
     }
 }
