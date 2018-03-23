878bd819044f9f981387a776942b3ed5463728ac
msramalho <up201403027@fe.up.pt>
2018-03-23 18:29:01 +0000

    project cleaned and ready for local 'database' for saving chunks and chunk info

--- proj1/Peer/src/localStorage/LocalChunk.java
+++ proj1/Peer/src/localStorage/LocalChunk.java
@@ -1,15 +1,24 @@
 package src.localStorage;
 
+import java.util.HashSet;
+
 public class LocalChunk {
     public transient LocalFile file;
-    public Integer chunkNo; // chunk ID
-    public Integer countAcks; // count the number of ACKs received for this chunk
     public transient byte[] chunk;
+
+    public Integer chunkNo; // chunk ID
+    // public Integer countAcks; // count the number of ACKs received for this chunk
+    public HashSet<Integer> peersAcks; //list of the ids of the peers that stored this chunk
 
     public LocalChunk(LocalFile file, Integer chunkNo, byte[] chunk) {
         this.chunkNo = chunkNo;
-        this.countAcks = 0;
         this.file = file;
         this.chunk = chunk;
     }
+
+    public void addPeerAck(Integer peerId) {
+        peersAcks.add(peerId);
+    }
+
+    public int getCountAcks() {return peersAcks.size();}
 }
--- proj1/Peer/src/main/Peer.java
+++ proj1/Peer/src/main/Peer.java
@@ -7,6 +7,9 @@ public class Peer {
     public static void main(String[] args) {
         PeerConfig peerConfig;
 
+        //TODO: local database next
+        System.out.println("TODO: local database is next");
+
         try {
             peerConfig = new PeerConfig(args);//create peer
         } catch (Exception e) {
@@ -22,33 +25,12 @@ public class Peer {
 
         //initiator peer, receives <filename> <replicationFactor>x
         if (args.length == 11) {
-            System.out.println("initiator");
+            System.out.println("[Peer] - initiator");
 
             // Calling LocalFile for testing
             LocalFile localFile = new LocalFile(args[9], Integer.parseInt(args[10]), peerConfig);
             localFile.splitFile();
-
-            //setup multicast socket and join group
-            //byte[] data = "This is the contents of the file".getBytes(); //create message with service port
-            //BackupChunk bcWorker = new BackupChunk(peerConfig, data, 1, 3);
-            //bcWorker.run();
-            //peerConfig.threadPool.submit(bcWorker);
         }
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        //TODO: create runnable for backupService and the others so that it is listening for changes in the peerConfig.mc****.mcQueue for requests to process - use threadpool, probably peerConfig will have access to the pool so that it assigns, decide how to conjugate the blocking deques and a dispatcher of tasks, should MulticastSocket call that??
-        /*try {
-            Message message = peerConfig.receiveMulticast(peerConfig.mcBackup);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }*/
-
 
     }
 }
