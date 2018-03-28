fd221d45c0eb51c0881548e9ccf9067181b970e2
Unknown <up201403027@fe.up.pt>
2018-03-28 15:50:06 +0100

    Stored worker is ready

--- proj1/Peer/src/worker/Stored.java
+++ proj1/Peer/src/worker/Stored.java
@@ -1,5 +1,8 @@
 package src.worker;
 
+import src.localStorage.Chunk;
+import src.localStorage.StoredChunk;
+
 // save information about what other peers are saying in the network so we can respect replication degrees
 // only receives STORED that are not about local files, those go to the worker.BackupChunk
 public class Stored extends Protocol {
@@ -10,6 +13,18 @@ public class Stored extends Protocol {
 
     @Override
     public void run() {
-        // d.peerConfig.internalState.updateExternalPutchunkReplies(d.message.fileId, d.message.chunkNo);
+        // check if this STORED is about a Stored Chunk
+        Chunk chunk = d.peerConfig.internalState.getStoredChunk(d.message);
+
+        // if not, check if this STORED is about a Local Chunk
+        if (chunk == null) chunk = d.peerConfig.internalState.getLocalChunk(d.message);
+
+        // if not, it is about a never-heard-off chunk, so it is not local, add as new (not locally saved) Stored Chunk
+        if (chunk == null) {
+            chunk = new StoredChunk(d.message);
+            d.peerConfig.internalState.addStoredChunk((StoredChunk) chunk );
+        }
+
+        chunk.addAck(d.message.senderId);
     }
 }
