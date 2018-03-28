1bc5f97c7e530386ca796068234c565265bbdfaa
Unknown <up201403027@fe.up.pt>
2018-03-28 09:47:16 +0100

    deleted storage, ignored some files and made peer.sh receive the peerId
    eg:
    $sh peer.sh 2
    where 2 is the peerID

--- proj1/Peer/peer.sh
+++ proj1/Peer/peer.sh
@@ -6,6 +6,11 @@ clear
 javac -cp .:./bin/:./ -d bin src/*/*.java
 echo "Source code compiled"
 
+# get peerID or use default
+PEERID="$1"
+PEERID=${PEERID:-"2"}  # If variable not set, use default.
+
+
 #Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
-java -cp .:./bin/ src.main.Peer 1.0 3 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
+java -cp .:./bin/ src.main.Peer 1.0 $PEERID 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
 
