2c7ad5d01cbdacaa698b8fe9faa896d46c9521d5
Unknown <up201403027@fe.up.pt>
2018-03-28 15:19:13 +0100

    updated bash scripts to have the second argument delete the internal state

--- proj1/Peer/initiator_peer.sh
+++ proj1/Peer/initiator_peer.sh
@@ -9,5 +9,12 @@ echo "Source code compiled"
 FILENAME="$1"
 FILENAME=${FILENAME:-"file.txt"}  # If variable not set, use default.
 
+# if the second argument equals 1 then delete the database
+if [ "$2" = "1" ]
+then
+    echo Deleting internal_state_peer_$PEERID.
+    rm -rf internal_state_peer_$PEERID
+fi
+
 #Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
 java -cp .:./bin/ src.main.Peer 1.0 1 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002 $FILENAME 2
--- proj1/Peer/peer.sh
+++ proj1/Peer/peer.sh
@@ -10,6 +10,12 @@ echo "Source code compiled"
 PEERID="$1"
 PEERID=${PEERID:-"2"}  # If variable not set, use default.
 
+# if the second argument equals 1 then delete the database
+if [ "$2" = "1" ]
+then
+    echo Deleting internal_state_peer_$PEERID.
+    rm -rf internal_state_peer_$PEERID
+fi
 
 #Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
 java -cp .:./bin/ src.main.Peer 1.0 $PEERID 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
