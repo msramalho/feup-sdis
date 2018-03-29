5b39ecabe0046cb6b5e518ab7f6d56309be795dc
Unknown <up201403027@fe.up.pt>
2018-03-29 14:41:25 +0100

    removed unnecessary public from interface

--- proj1/Peer/src/main/InitiatorPeer.java
+++ proj1/Peer/src/main/InitiatorPeer.java
@@ -1,16 +1,17 @@
 package src.main;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface InitiatorPeer extends Remote {
 
-    public void backup(String file, int ack) throws RemoteException;
+    void backup(String file, int ack) throws RemoteException;
 
-    public void restore(String file) throws RemoteException;
+    void restore(String file) throws RemoteException;
 
-    public void delete(String file) throws RemoteException;
+    void delete(String file) throws RemoteException;
 
-    public void reclaim(int rec) throws RemoteException;
+    void reclaim(int rec) throws RemoteException;
 
-    public void state() throws RemoteException;
+    void state() throws RemoteException;
 }
