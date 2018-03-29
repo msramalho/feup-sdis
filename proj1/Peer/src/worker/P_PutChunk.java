591ffae975a2e0a328d43233a19651f1378f0bce
Unknown <up201403027@fe.up.pt>
2018-03-29 23:48:17 +0100

    removed comment

--- proj1/Peer/src/worker/P_PutChunk.java
+++ proj1/Peer/src/worker/P_PutChunk.java
@@ -9,10 +9,9 @@ public class P_PutChunk extends Protocol {
 
     @Override
     public void run() {
-        if (d.message.body.length == 0) {
-            System.out.println("received chunk with length 0, the last");
-            return;
-        }
+        //received chunk with length 0, the last
+        if (d.message.body.length == 0) return;
+        
         // try to read the chunk from the internal state, and add it if it is not there
         StoredChunk sChunk = d.peerConfig.internalState.getStoredChunk(d.message);
         if (sChunk == null) {
