fc84af09e343e63012ddaf3a2c9c9fa32995e388
Unknown <up201403027@fe.up.pt>
2018-03-29 22:22:35 +0100

    Enhancement 1 completed

--- proj1/Peer/src/worker/Protocol.java
+++ proj1/Peer/src/worker/Protocol.java
@@ -11,9 +11,23 @@ public abstract class Protocol {
 
     public abstract void run();
 
-    public void sleepRandom(){
+    /**
+     * sleep time is longer for Peers with lower percentage of disk space occupied
+     * for a peer with 0% space occupied, interval is random between [0, 400]
+     * for a peer with 100% space occupied, interval is random between [400, 400]
+     */
+    public void sleepRandomConsiderDiskSpace() {
+        int percentOccupied = (int) (100 * (d.peerConfig.internalState.occupiedSpace / d.peerConfig.internalState.allowedSpace));
+        percentOccupied = Math.min(percentOccupied, 100); // if memory exceeds 100%
+        System.out.println(String.format("[Protocol:%9s] - percentOccupied %d = min %d ms", d.message.action, percentOccupied, 4 * percentOccupied));
+        sleepRandom(4 * percentOccupied);
+    }
+
+    public void sleepRandom() {sleepRandom(0);}
+
+    public void sleepRandom(int from) {
         try {
-            int sleepFor = ThreadLocalRandom.current().nextInt(10, 401);
+            int sleepFor = ThreadLocalRandom.current().nextInt(from, 401);
             System.out.println(String.format("[Protocol:%9s] - sleep for %3d ms", d.message.action, sleepFor));
             Thread.sleep(sleepFor);
         } catch (InterruptedException e) {
