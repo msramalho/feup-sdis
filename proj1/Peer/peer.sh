7233316ff4a390d5b3966042d7c493b356c8e752
Unknown <up201403027@fe.up.pt>
2018-04-02 23:22:05 +0100

    revert multicast group IP

--- proj1/Peer/peer.sh
+++ proj1/Peer/peer.sh
@@ -30,6 +30,6 @@ VERSION="$4"
 VERSION=${VERSION:-"1.0"}  # If variable not set, use default.
 
 #Usage: <protocolVersion> <peerId> <serviceAccessPoint> <mccIP> <mccPort> <mdbIp> <mdbPort> <mdrIp> <mdrPort>
-java -cp .:./bin/ src.main.Peer $VERSION $PEERID 8499 234.234.234.234 1234 234.234.234.235 1235 234.234.234.236 1236
+java -cp .:./bin/ src.main.Peer $VERSION $PEERID 8499 224.0.0.0 9000 224.0.0.1 9001 224.0.0.2 9002
 
 # Usage: <peerId:default=1> <resetDatabase:default=false> <startRMI:default=false>
