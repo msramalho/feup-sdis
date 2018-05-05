317f5d6fadfb75af07a0973f6c04a2364e4cb366
msramalho <up201403027@fe.up.pt>
2018-05-05 17:47:54 +0100

    closes #15 with some comments

--- proj2/Peer/src/util/MulticastChannels.java
+++ proj2/Peer/src/util/MulticastChannels.java
@@ -9,6 +9,9 @@ public class MulticastChannels {
     public MulticastSocketC backup;  // Multicast Back Up Socket
     public MulticastSocketC restore; // Multicast Restore Socket
 
+    /**
+     * explicit constructor
+     */
     public MulticastChannels(PeerConfig peerConfig, String mccIp, Integer mccPort, String mcbIp, Integer mcbPort, String mcrIp, Integer mcrPort) throws IOException {
         //setup sockets and join group for <mccIP> <mccPort>, <mdbIp> <mdbPort> and <mdrIp> <mdrPort>, respectively
         control = new MulticastSocketC(mccIp, mccPort, "MCControl", peerConfig);
@@ -16,16 +19,25 @@ public class MulticastChannels {
         restore = new MulticastSocketC(mcrIp, mcrPort, "MCRestore", peerConfig);
     }
 
+    /**
+     * Constructor from the command line arguments
+     */
     public MulticastChannels(PeerConfig peerConfig, String[] args) throws IOException {
         this(peerConfig, args[3], Integer.parseInt(args[4]), args[5], Integer.parseInt(args[6]), args[7], Integer.parseInt(args[8]));
     }
 
+    /**
+     * start the listing actions on the threads
+     */
     public void listen() {
         (new Thread(control)).start();
         (new Thread(backup)).start();
         (new Thread(restore)).start();
     }
 
+    /**
+     * kill the listening threads
+     */
     public void stop() {
         control.stop();
         control.stop();
