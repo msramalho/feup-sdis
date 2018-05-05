a93ee500278c662c29d58fd47608eb39b8b7e4e3
msramalho <up201403027@fe.up.pt>
2018-05-05 17:54:08 +0100

    prettier logger

--- proj2/Peer/src/util/Logger.java
+++ proj2/Peer/src/util/Logger.java
@@ -1,7 +1,6 @@
 package src.util;
 
 public class Logger {
-    private static int maxSize = 0;
     private String context;
 
     /**
@@ -26,9 +25,8 @@ public class Logger {
     private String format(String message) {
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
 
-        maxSize = Math.max(maxSize, context.length());
         // the 3 means this is called from 3 -> 2 (print) -> 1 (format) ->  0 (getStackTrace)
         StackTraceElement relevant = stackTraceElements[3];
-        return String.format("[%" + maxSize + "s->%s:%3d] - %s", context, relevant.getMethodName(), relevant.getLineNumber(), message);
+        return String.format("[%s->%s:%3d] - %s", context, relevant.getMethodName(), relevant.getLineNumber(), message);
     }
 }
