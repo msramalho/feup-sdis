459622395dbbe2536b9f9412845f1fb408549d2d
msramalho <up201403027@fe.up.pt>
2018-05-05 20:54:24 +0100

    base cluster class

---
+++ proj2/Peer/src/main/Cluster.java
@@ -0,0 +1,17 @@
+package src.main;
+
+import src.util.MulticastChannels;
+
+import java.util.HashSet;
+
+public class Cluster {
+    public static int MAX_SIZE = 4; // the maximum number of peers in a cluster
+    int id; // the unique identifier of this cluster
+    int depth; // the depth at which it is located (starts at 0) - can be inferred from the clusters arraylist, but this is useful
+    public MulticastChannels multicast;
+    public HashSet<Integer> peers;
+
+    public void clearPeers() {
+        peers = new HashSet<>();
+    }
+}
