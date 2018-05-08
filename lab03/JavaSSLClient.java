7bba30aaae91d18a63aff697a69cb66dd9869cd6
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-05-08 12:17:05 +0100

    easy way of communication, only hankshake missing

--- lab03/JavaSSLClient.java
+++ lab03/JavaSSLClient.java
@@ -1,86 +1,54 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.net.*;
+import java.security.*;
 import javax.net.ssl.SSLSocketFactory;
-import javax.crypto.KeyGenerator;
-import java.security.SecureRandom;
-import java.security.Key;
-import javax.crypto.spec.SecretKeySpec;
-import java.*;
-import javax.crypto.Cipher;
- 
-// RUN CMD
-// java -Djavax.net.ssl.trustStore=/home/diogo/Github/feup-sdis/lab03/mykeystore/examplestore -Djavax.net.ssl.trustStorePassword=sdis18 JavaSSLClient localhost 5000 + 1,1,1 AES
+import com.sun.net.ssl.SSLContext;
+import com.sun.net.ssl.TrustManagerFactory;
+import com.sun.net.ssl.TrustManager;
 
-public class JavaSSLClient {
-    
-    // Default Port 
-    static final int port = 8000;
- 
-    public static void main(String[] args) {
-        
-        String host = args[0];
-        // User's PORT
-        int port = Integer.parseInt(args[1]);
-        String oper = args[2];
-        String opnd = args[3];
-        String[] list = opnd.split(",");
-        String cypher = args[4];
+// javac -Xlint:deprecation JavaSSLClient.java
 
-        System.setProperty("javax.net.ssl.trustStore","/home/diogo/Github/feup-sdis/lab03/mykeystore/examplestore");
-        System.setProperty("javax.net.ssl.trustStorePassword","sdis18");
+public class JavaSSLClient{
 
-        SSLSocketFactory sslSocketFactory =  (SSLSocketFactory)SSLSocketFactory.getDefault();
-        
+  private static final String HOST = "localhost";
 
-        try {
-            Socket socket = sslSocketFactory.createSocket("localhost", port);
-            //socketStrem to receive server communication
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            
-            try (BufferedReader bufferedReader = 
-                    new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                Scanner scanner = new Scanner(System.in);
-                
-                while(true){
-                    System.out.println("Enter Message:");
-                    
-                    String inputLine = scanner.nextLine();
+  private static final int PORT = 8080;
 
-                    if(inputLine.equals("q")){
-                        break;
-                    }
-                     
-                    
-                    out.println(inputLine);
-                    System.out.println(bufferedReader.readLine());
-                }
-            }
-             
-        } catch (IOException ex) {
-            Logger.getLogger(JavaSSLClient.class.getName())
-                    .log(Level.SEVERE, null, ex);
-        }     
+  public static void main(String[] args) throws Exception {
+    System.setProperty("javax.net.ssl.trustStore","/home/diogo/Github/feup-sdis/lab03/mykeystore/examplestore");
+    System.setProperty("javax.net.ssl.trustStorePassword","sdis18");
+
+    char[] passphrase = "sdis18".toCharArray();
+    KeyStore keystore = KeyStore.getInstance("JKS");
+    keystore.load(new FileInputStream("/home/diogo/Github/feup-sdis/lab03/mykeystore/examplestore"), passphrase);
+
+    //This class acts as a factory for trust managers based on a source of trust material. 
+    //Each trust manager manages a specific type of trust materia. It uses keystore
+    TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+    tmf.init(keystore);
+
+    SSLContext context = SSLContext.getInstance("TLS");
+    TrustManager[] trustManagers = tmf.getTrustManagers();
+
+    context.init(null, trustManagers, null);
+
+    SSLSocketFactory sf = context.getSocketFactory();
+
+    Socket s = sf.createSocket(HOST, PORT);
+    OutputStream out = s.getOutputStream();
+    out.write("\nConnection established.\n\n".getBytes());
+
+    int theCharacter = 0;
+    theCharacter = System.in.read();
+    while (theCharacter != '~') // The '~' is an escape character to exit
+    {
+      out.write(theCharacter);
+      out.flush();
+      theCharacter = System.in.read();
     }
 
-
-    /*private boolean testSSLSocket(SSLSocket sslSocket) {
-       try {
-        if(DEBUG_ALL) System.out.println("Starting SSL handshake");
-        sslSocket.setSoTimeout(0);
-        sslSocket.startHandshake();
-           return true;
-       } catch (Exception e) {
-        if(DEBUG_ALL) e.printStackTrace();
-           return false;
-       }
-    }*/
-   
-     
+    out.close();
+    s.close();
+  }
 }
