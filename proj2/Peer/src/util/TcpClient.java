5237205a6ab1967ca028360e38d79d0d7faf69d5
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-05-09 22:26:04 +0100

    connection stablished - SSL server-client

--- proj2/Peer/src/util/TcpClient.java
+++ proj2/Peer/src/util/TcpClient.java
@@ -10,19 +10,31 @@ public class TcpClient extends Tcp {
     public TcpClient(Message message) {
         
         AbstractMap.SimpleEntry<String, Integer> tcpCoordinates = message.getTCPCoordinates();
+
+        SSLSocketFactory sf = null;
+
         try{
             char[] passphrase = "sdis18".toCharArray();
             KeyStore keystore = KeyStore.getInstance("JKS");
-            keystore.load(new FileInputStream("/home/diogo/Github/feup-sdis/proj2/Peer/src/util/mykeystore/examplestore"), passphrase);  
+            keystore.load(new FileInputStream("/home/diogo/Github/feup-sdis/proj2/Peer/src/util/mykeystore/examplestore"), passphrase);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(keystore);
+            SSLContext context = SSLContext.getInstance("TLS");
+            TrustManager[] trustManagers = tmf.getTrustManagers();
+            context.init(null, trustManagers, null);
+            sf = context.getSocketFactory();
+
         } catch (Exception e){
-                logger.err("Cant find File " + e.getMessage());
+            logger.err("Cant find File " + e.getMessage());
         }     
         
         try {
-            System.out.println("TCP CLIENT");
-            socket = new Socket(tcpCoordinates.getKey(), tcpCoordinates.getValue());  
+            socket = sf.createSocket(tcpCoordinates.getKey(), tcpCoordinates.getValue()); 
+            System.out.println("[TCP Client] - SSL Connected and Message Sent...");
+
         } catch (IOException e) {
-            logger.err("Unable to open TCP socket: " + e.getMessage());
+            logger.err("Unable to open TCP Via SSLsocket: " + e.getMessage());
         }
     }
 
